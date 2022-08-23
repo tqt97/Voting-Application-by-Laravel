088b9adbbe788855a3b335ffe00a48c506385835
TuanDev <tuandev@TuanDevs-MacBook-Pro.local>
2022-08-23 13:45:37 +0700

    Add idea page

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('index');
 });
 
+Route::view('/idea', 'show');
+
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
 // })->middleware(['auth'])->name('dashboard');
